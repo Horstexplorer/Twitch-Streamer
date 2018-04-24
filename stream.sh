6ed719c84958b2b5a9d8902bc43206af6d1cc132
Horstexplorer <horstexplorerlps@gmail.com>
2018-04-24 19:48:11 +0200

    Create stream.sh

---
+++ stream.sh
@@ -0,0 +1,30 @@
+echo "Checking existing files..."
+
+if [ -f temp.txt ]
+then
+  echo "Removing temp.txt"
+  rm -R temp.txt
+fi
+if [ -f playlist.txt ]
+then
+  echo "Removing playlist.txt"
+  rm -R playlist.txt
+fi
+if [ -f stream.mp4 ]
+then
+  echo "Removing stream.mp4"
+  rm stream.mp4
+fi
+
+echo "Generating new playlist..."
+find -type f -name *.mp4 >> temp.txt
+while read line; do echo "file '${line:2}'" >> playlist.txt ;done < temp.txt
+rm -R temp.txt
+
+echo "Concatenating files..."
+ffmpeg -f concat -i playlist.txt -c copy stream.mp4
+
+echo "Going online..."
+ffmpeg -i stream.mp4 -vcodec libx264 -crf 30 -preset medium -r 30 -f flv rtmp://live.twitch.tv/app/<<streamkey>>
+
+# Replace <<streamkey>> with your stream key for twitch
