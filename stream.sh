345058268b5229c82aa31c08d011aa50bef0e601
Horstexplorer <horstexplorerlps@gmail.com>
2018-04-25 12:53:51 +0200

    Update stream.sh
    +some colors
    +options to controll bitrate
    +output framerate = input framerate

--- stream.sh
+++ stream.sh
@@ -1,30 +1,31 @@
-echo "Checking existing files..."
+echo -e "[\033[1;32mInfo\033[0m] Checking existing files..."
 
 if [ -f temp.txt ]
 then
-  echo "Removing temp.txt"
+  echo -e "[\033[1;33mDEL\033[0m] Removing temp.txt"
   rm -R temp.txt
 fi
 if [ -f playlist.txt ]
 then
-  echo "Removing playlist.txt"
+  echo -e "[\033[1;33mDEL\033[0m] Removing playlist.txt"
   rm -R playlist.txt
 fi
 if [ -f stream.mp4 ]
 then
-  echo "Removing stream.mp4"
+  echo -e "[\033[1;33mDEL\033[0m] Removing stream.mp4"
   rm stream.mp4
 fi
 
-echo "Generating new playlist..."
-find -type f -name *.mp4 >> temp.txt
+echo -e "[\033[1;32mInfo\033[0m] Generating new playlist..."
+find -type f -name '*.mp4' >> temp.txt
 while read line; do echo "file '${line:2}'" >> playlist.txt ;done < temp.txt
 rm -R temp.txt
 
-echo "Concatenating files..."
+echo -e "[\033[1;32mInfo\033[0m] Concatenating files..."
 ffmpeg -f concat -i playlist.txt -c copy stream.mp4
 
-echo "Going online..."
-ffmpeg -stream_loop -1 -i stream.mp4 -vcodec libx264 -crf 30 -preset medium -tune film  -tune fastdecode -r 30 -f flv rtmp://live.twitch.tv/app/<<streamkey>>
+echo -e "[\033[1;32mInfo\033[0m] Going online..."
+sleep 5
+ffmpeg -stream_loop -1 -re -i stream.mp4 -vcodec libx264 -crf 30 -preset medium -tune film  -tune fastdecode -b:v 3M -maxrate 8M -minrate 2M -bufsize 2M -ar 44100 -f flv rtmp://live.twitch.tv/app/<<streamkey>>
 
 # Replace <<streamkey>> with your stream key for twitch
