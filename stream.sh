b73fedf012823ea8cac7343bb2519096f92a25c7
Horstexplorer <horstexplorerlps@gmail.com>
2018-04-25 23:43:40 +0200

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -22,10 +22,11 @@ while read line; do echo "file '${line:2}'" >> playlist.txt ;done < temp.txt
 rm -R temp.txt
 
 echo -e "[\033[1;32mInfo\033[0m] Concatenating files..."
-ffmpeg -f concat -i playlist.txt -c copy stream.mp4
+ffmpeg -err_detect ignore_err -loglevel panic -f concat -i playlist.txt -c copy stream.mp4
 
 echo -e "[\033[1;32mInfo\033[0m] Going online..."
 sleep 5
-ffmpeg -stream_loop -1 -re -i stream.mp4 -vcodec libx264 -crf 30 -preset medium -tune film  -tune fastdecode -b:v 3M -maxrate 8M -minrate 2M -bufsize 2M -ar 44100 -f flv rtmp://live.twitch.tv/app/<<streamkey>>
+ffmpeg -err_detect ignore_err -stream_loop -1 -re -i stream.mp4 -vcodec libx264 -crf 30 -preset medium -tune film  -tune fastdecode -b:v 3M -maxrate 8M -minrate 2M -bufsize 2M -ar 44100 -f flv rtmp://live.twitch.tv/app/<<streamkey>>
+#Things like "Non-monotonous DTS in output stream" or "Past duration x.xxxxxx too large" occure sometimes. Not sure why but I try to fix it soon.
 
 # Replace <<streamkey>> with your stream key for twitch
